2ec87e6636d79e6e390aca8d6242e2a9d6b1b3b8
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2021-04-15 17:49:23 +0900

    feat: add logger, fix interpreter

--- server/http/http.py
+++ server/http/http.py
@@ -1,32 +1,36 @@
 import socket
 import asyncio
 import logging
+import json
 from email.utils import formatdate
-from .const import reason_phrase
-from .utils import parse_http_req, HttpRequest, interpret
+from .utils import parse_http_req, HttpRequest, interpret, logger, HttpRespBuilder
 
 
-class HttpReqHandler:
+class HttpReqHandler():
+
     def __init__(self, request: HttpRequest):
         self.request = request
 
+    @logger
     def __on_get(self):
         return HttpRespBuilder(200) \
             .add_header('Server', 'ProChat') \
             .add_header('Content-Type', 'text/html; charset=utf-8') \
             .add_header('Access-Control-Allow-Origin', '*') \
             .add_header('Date', formatdate(timeval=None, localtime=False, usegmt=True)) \
-            .add_to_body("<html><body>Hello coder!</body></html>")\
+            .add_to_body("<html><body>Hello coder!</body></html>") \
             .compile()
 
+    @logger
     def __on_post(self):
-        result = interpret(self.request.body)
+        code = json.loads(self.request.body).get("code")
+        result = interpret(code)
         return HttpRespBuilder(200) \
             .add_header('Server', 'ProChat') \
             .add_header('Content-Type', 'text/html; charset=utf-8') \
             .add_header('Access-Control-Allow-Origin', '*') \
             .add_header('Date', formatdate(timeval=None, localtime=False, usegmt=True)) \
-            .add_to_body(f"<html><body>The result is {str(result)}</body></html>") \
+            .add_to_body(f"<html><body>Your message is:<br>{str(result)}</body></html>") \
             .compile()
 
     def handle(self):
@@ -37,37 +41,16 @@ class HttpReqHandler:
         return HttpRespBuilder(501).compile()
 
 
-class HttpRespBuilder:
-    def __init__(self, status_code):
-        self.headers = {}
-        self.body = b""
-        self.status = f"HTTP/1.1 {status_code} {reason_phrase.get(status_code, 500)}\r\n".encode()
-
-    def add_header(self, key: str, value: str):
-        self.headers[key] = value
-        return self
-
-    def add_to_body(self, content):
-        self.body = self.body + str(content).encode()
-        return self
-
-    def compile(self):
-        concat_headers = b"".join([f"{k}: {self.headers[k]}\r\n".encode() for k in self.headers])
-        return b"".join([self.status, concat_headers, b"\r\n", self.body])
-
-
 class Session:
     def __init__(self, sock, addr):
         self.sock = sock
         self.addr = addr
+        logging.info(f"Coming Request from {addr}")
 
     async def resolve(self):
         payload = self.sock.recv(1024).decode()
-        logging.info(payload)
-
-        parsed = parse_http_req(payload)
-        resp = HttpReqHandler(parsed).handle()
-
+        req = parse_http_req(payload)
+        resp = HttpReqHandler(req).handle()
         self.sock.send(resp)
         self.sock.close()
 
@@ -81,6 +64,7 @@ class HttpServer:
         http_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         http_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         http_socket.bind((cls.HTTP_HOST, cls.HTTP_PORT))
+        logging.info(f"Server started on port {cls.HTTP_PORT}")
         http_socket.listen(1)
         while True:
             c_conn, c_addr = http_socket.accept()
--- server/http/utils.py
+++ server/http/utils.py
@@ -1,4 +1,7 @@
-import json
+import ast
+import logging
+
+from server.http.const import reason_phrase
 
 
 class HttpRequest:
@@ -10,6 +13,25 @@ class HttpRequest:
         self.body = body
 
 
+class HttpRespBuilder:
+    def __init__(self, status_code):
+        self.headers = {}
+        self.body = b""
+        self.status = f"HTTP/1.1 {status_code} {reason_phrase.get(status_code, 500)}\r\n".encode()
+
+    def add_header(self, key: str, value: str):
+        self.headers[key] = value
+        return self
+
+    def add_to_body(self, content):
+        self.body = self.body + str(content).encode()
+        return self
+
+    def compile(self):
+        concat_headers = b"".join([f"{k}: {self.headers[k]}\r\n".encode() for k in self.headers])
+        return b"".join([self.status, concat_headers, b"\r\n", self.body])
+
+
 def parse_http_req(req: str):
     start_line, header_body = req.split('\r\n', 1)
     headers, body = header_body.split('\r\n\r\n', 1)
@@ -19,8 +41,20 @@ def parse_http_req(req: str):
 
 
 def interpret(code):
-    parsed = json.loads(code)
-    stmt = parsed.get('stmt')
-    exec(stmt)
-    expr = parsed.get('expr')
-    return eval(expr)
+    block = ast.parse(code, mode='exec')
+    last = ast.Expression(block.body.pop().value)
+    _globals, _locals = {}, {}
+    exec(compile(block, '<string>', mode='exec'), _globals, _locals)
+    return eval(compile(last, '<string>', mode='eval'), _globals, _locals)
+
+
+def logger(func):
+    def handle(*args, **kwargs):
+        try:
+            ret = func(*args, **kwargs)
+            return ret
+        except Exception as e:
+            logging.error(str(e))
+            return HttpRespBuilder(500).compile()
+
+    return handle
